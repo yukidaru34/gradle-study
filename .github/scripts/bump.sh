80df318020951c688c2ca1f1f095e246852f8e12
“yuuking0304” <aikawa.yuki@tis.co.jp>
2024-09-08 01:07:14 +0900

    コロン廃止

--- .github/scripts/bump.sh
+++ .github/scripts/bump.sh
@@ -13,18 +13,18 @@ PREFIX=$1
 OCTET=$2
 
 # 現在のタグ一覧を取得し、正規表現で該当するプレフィックスのタグを検索
-LATEST_TAG=$(git tag -l "${PREFIX}*" | grep -E "^${PREFIX}:[0-9]+\.[0-9]+\.[0-9]+snapshot$" | sort -V | tail -n 1)
+LATEST_TAG=$(git tag -l "${PREFIX}*" | grep -E "^${PREFIX}-[0-9]+\.[0-9]+\.[0-9]+snapshot$" | sort -V | tail -n 1)
 
 # タグが存在しない場合の処理
 if [ -z "$LATEST_TAG" ]; then
-  echo "該当するタグが見つかりませんでした。${PREFIX}0.1.0 を作成します。"
-  NEW_TAG="${PREFIX}:0.1.0-snapshot"
+  echo "該当するタグが見つかりませんでした。${PREFIX}-0.1.0 を作成します。"
+  NEW_TAG="${PREFIX}-0.1.0-snapshot"
   NEW_VERSION="0.1.0-snapshot"
 else
   # 最新のタグのバージョン番号を分割
   echo "最新のタグは ${LATEST_TAG} です。"
 
-  VERSION=$(echo "$LATEST_TAG" | sed "s/^${PREFIX}://")
+  VERSION=$(echo "$LATEST_TAG" | sed "s/^${PREFIX}-//")
   MAJOR=$(echo "$VERSION" | cut -d. -f1)
   MINOR=$(echo "$VERSION" | cut -d. -f2)
   PATCH=$(echo "$VERSION" | cut -d. -f3)
@@ -49,7 +49,7 @@ else
   esac
 
   # 新しいタグを作成
-  NEW_TAG="${PREFIX}:${MAJOR}.${MINOR}.${PATCH}-snapshot"
+  NEW_TAG="${PREFIX}-${MAJOR}.${MINOR}.${PATCH}-snapshot"
   NEW_VERSION="${MAJOR}.${MINOR}.${PATCH}-snapshot"
 fi
 
